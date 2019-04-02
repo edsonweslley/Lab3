ac8dba7b4a250e87698804e0f949ff3ad67a3e76
118210140 GABRIEL DANTAS SANTOS DE AZEVEDO <gabrieldsa@lcc3-113.lcc.ufcg.edu.br>
2019-04-02 09:28:31 -0300

    first commit

---
+++ src/Lab3GabrielDantas/Contato.java
@@ -0,0 +1,28 @@
+package Lab3GabrielDantas;
+import java.util.Scanner;
+
+public class Contato {
+	
+	private String nome;
+	private String sobrenome;
+	private String telefone;
+	
+	public void cadastrar(String nome, String sobrenome,String telefone) {
+		this.nome = nome;
+		
+		
+	}
+	public void listar() {
+		
+	}
+	public void exibir() {
+		
+	}
+	public void sair() {
+		
+	}
+	public void opcao( ) {
+		
+	}
+	
+}
---
+++ src/Lab3GabrielDantas/Contatos.java
@@ -0,0 +1,16 @@
+package Lab3GabrielDantas;
+import java.util.Scanner;
+
+public class Contatos {
+	
+
+	public static void main(String[] args) {
+		System.out.println("(C)adastrar Contato");
+		System.out.println("(L)istar Contato");
+		System.out.println("(E)xibir Contato");
+		System.out.println("(S)air"+"\n");
+		System.out.print("Opção> ");
+
+	}
+
+}
